916830de56ca59f5c4cf4621dc00d96fa8cb2430
BilalOrabi <bilalorabi99@gmail.com>
2026-04-28 11:24:49 +0300

    adding the codex

---
+++ The_Codex/alchemy/__init__.py
@@ -0,0 +1,4 @@
+from .elements import create_air # from part 1
+from .potions import healing_potion as heal # from part 2
+from .potions import strength_potion # from part 2
+from .transmutation import lead_to_gold # from part 3
